cf94e7a7e5877dab62ef2e82804306b13a4ee72a
bless-stanford <107686196+bless-stanford@users.noreply.github.com>
2024-04-12 16:09:44 -0700

    first commit

---
+++ lib/prisma.ts
@@ -0,0 +1,16 @@
+// prismaClient.ts
+import { PrismaClient } from '@prisma/client';
+
+class PrismaSingleton {
+  private static instance: PrismaClient;
+
+  public static getInstance(): PrismaClient {
+    if (!PrismaSingleton.instance) {
+      PrismaSingleton.instance = new PrismaClient();
+    }
+
+    return PrismaSingleton.instance;
+  }
+}
+
+export const prisma = PrismaSingleton.getInstance();
---
+++ src/pages/api/register-new-user.ts
@@ -0,0 +1,54 @@
+// next
+import { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from 'lib/prisma';
+// utils
+import cors from 'src/utils/cors';
+
+async function registerNewUser(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { userId, data } = req.body;
+    const { email, displayName } = data;
+
+    const user = await prisma.user.create({
+      data: {
+        id: userId,
+        email,
+        displayName,
+      },
+    });
+
+    res.status(200).json({ user });
+  } catch (error) {
+    console.error('Error creating user:', error);
+    res.status(500).json({ error: 'Error creating user' });
+  }
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    await cors(req, res);
+
+    switch (req.method) {
+      case 'GET':
+        break;
+      case 'POST':
+        await registerNewUser(req, res);
+        break;
+      case 'PUT':
+        break;
+      case 'PATCH':
+        break;
+      case 'DELETE':
+        break;
+      default:
+        res.status(405).json({
+          message: 'Method not allowed',
+        });
+    }
+  } catch (error) {
+    console.error('[REGISTER API]: ', error);
+    res.status(500).json({
+      message: 'Internal server error',
+    });
+  }
+}
