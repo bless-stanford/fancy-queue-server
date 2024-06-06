584c4d734c7cd7535e3733c6e82b50d9ce85df31
Agustin Otero <agotero@stanford.edu>
2024-06-06 11:48:13 -0700

    backup

--- src/pages/api/queues.ts
+++ src/pages/api/queues.ts
@@ -11,11 +11,42 @@ import dayjs from 'dayjs';
 import { prisma } from 'lib/prisma';
 // utils
 import cors from 'src/utils/cors';
+import { forEach } from 'lodash';
+
+// takes a 2d list of requests and gives me the n earliest ones
+function getFirst(list: any[], n: number) {
+
+  const allRequests = [];
+
+  for (let i = 0; i < list.length; i++) {
+    for (let j = 0; j < list[i].length; j++) {
+      allRequests.push(list[i][j]);
+    }
+  }
+
+  allRequests.sort((req1: any, req2: any) => {
+    return req1.timeJoined.getTime() - req2.timeJoined.getTime();
+  })
+
+  return allRequests.slice(0, n);
+}
+
+//make sure to deal with string int stuff in the database
+function removeReqsOfType(type: any) {
+  return function(value: { problemType: any; }, index: any, arr: any[]) {
+    if (value.problemType == parseInt(type)) {
+      arr.splice(index, 1);
+      return true;
+    }
+    return false;
+  }
+}
 
 async function createQueue(req: NextApiRequest, res: NextApiResponse) {
   try {
     const { courseId, data } = req.body;
     const { email, numberOfWeeks, day, startTime, endTime, helpers } = data;
+
 
     const startTimeParsed = dayjs(startTime, 'HH:mm');
     const endTimeParsed = dayjs(endTime, 'HH:mm');
@@ -171,12 +202,16 @@ async function leaveQueue(req: NextApiRequest, res: NextApiResponse) {
 // should probably call this open queue so with some check (eg, if already open throw error.)
 async function toggleQueue(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const { userId, queueId } = req.body;
+    const { userId, queueId, data} = req.body;
+    const percentFilter = data
 
+    console.log(data);
+    console.log(userId);
     if (!userId || !queueId) {
       return res.status(400).json({ error: 'Missing required fields' });
     }
 
+    console.log(queueId);
     const queue = await prisma.queue.findUnique({
       where: { id: queueId },
     });
@@ -185,11 +220,123 @@ async function toggleQueue(req: NextApiRequest, res: NextApiResponse) {
       return res.status(404).json({ error: 'Queue not found' });
     }
 
-    // Update the queue to open
-    await prisma.queue.update({
+    // all the queues that are open for this course
+    const queues = await prisma.queue.findMany({
+      where: {
+        courseId: queue.courseId,
+        isOpen: true
+      }
+    });
+
+     // Update the queue to open
+     await prisma.queue.update({
       where: { id: queueId },
       data: { isOpen: !queue.isOpen },
     });
+
+    // if there is more than 1, redistribute prorated by the percentFilter
+    if (queues.length > 0 && !queue.isOpen) {
+      let totalReq = 0
+      const requestsList = []
+      for (let i = 0; i < queues.length; i++) {
+        const requests = await prisma.request.findMany({
+          where: {
+            queueId: queues[i].id
+          }
+        });
+        requestsList.push(requests)
+        totalReq += requests.length
+      }
+
+      const n = percentFilter * totalReq
+      console.log(requestsList);
+      let requests = getFirst(requestsList, n);
+      // potentially sort them here by timeJoined
+
+      //get rid of everything in prisma from requests
+      for (let i = 0; i < requests.length; i++) {
+          const deletedReq = await prisma.request.delete({
+            where: {
+              id: requests[i].id
+            }
+          });
+      }
+      console.log(n);
+
+      //distribute based on specialty
+      queues.push(queue)
+      const newReqs = []
+      for (let i = 0; i < queues.length; i++) {
+        // get the owner of each queue
+        const user = await prisma.user.findFirst({
+          where: {
+            id: queues[i].userId
+          }
+        });
+
+        // sort requests by specialty
+        newReqs.push(requests.filter(removeReqsOfType(user?.specialty)));
+      }
+
+      console.log("requests moving");
+      console.log(newReqs);
+      // create new requests by specialty
+      for (let i = 0; i < newReqs.length; i++) {
+        for (let j = 0; j < newReqs[i].length; j++) {
+          const newreq  = await prisma.request.create({
+            data: {
+              info: newReqs[i][j].info,
+              displayName: newReqs[i][j].displayName,
+              problemType: newReqs[i][j].problemType,
+              timeJoined: newReqs[i][j].timeJoined,
+              queue: {
+                connect: {
+                  id: queues[i].id as string
+                },
+              },
+              user: {
+                connect: {
+                  id: newReqs[i][j].userId as string
+                },
+              },
+            },
+          });
+        }
+      }
+
+      //add the leftovers balancing the remainders
+      while (requests.length > 0) {
+        let smallest = 0;
+        let index = 0;
+        for (let i = 0; i < newReqs.length; i++) {
+          if (newReqs[i].length < smallest) {
+            smallest = newReqs.length
+            index = i;
+          }
+        }
+
+        const req = requests.pop();
+        const newreq  = await prisma.request.create({
+          data: {
+            info: req.info,
+            displayName: req.displayName,
+            problemType: req.problemType,
+            timeJoined: req.timeJoined,
+            queue: {
+              connect: {
+                id: queues[index].userId as string
+              },
+            },
+            user: {
+              connect: {
+                id: req.userId as string
+              },
+            },
+          },
+        });
+        newReqs[index].push(newreq)
+      }
+    }
 
     return res.status(200).json({ message: 'Successfully opened the queue' });
   } catch (error) {
